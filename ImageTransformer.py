e08934872e4f80ccd96ba03e3fb909a57ca3bb50
yaroslavtsepkov <yaroslav.tsepkov@gmail.com>
2021-09-27 11:08:38 +0400

    add gitignore

--- ImageTransformer.py
+++ ImageTransformer.py
@@ -1,30 +1,42 @@
 from tqdm.auto import tqdm
 import os
-from glob import glob
 import torchvision.transforms as T
 from PIL import Image
 from argparse import ArgumentParser
 
 
-def processing(filepath:str):
+def processing(filepath:str)->list:
     img = Image.open(filepath)
     h, w = img.size
     if h > 512 and w > 512:
-        img = T.CenterCrop(h, h)(img)
+        img = T.CenterCrop((h//2,w//2))(img)
         imgs = [T.RandomCrop(256)(img) for _ in range(10)]
-    return imgs
+        imgs = [T.RandomVerticalFlip()(img) for img in imgs]
+        return imgs
     
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--path", help="dir with images", type=str)
+    parser.add_argument("--path", default="/home/yaroslav/repos/research-work/DATA/TIBET_BANDS_B", help="path to dir with images", type=str)
     parser.add_argument("--ext", help="png, jpeg, tif, ...", type=str)
+    parser.add_argument("--opath", default = "/home/yaroslav/repos/research-work/DATA/TIBET", help="output path ti dir", type=str)
     args = parser.parse_args()
-    for pathfile in tqdm(glob(f"{args.path}/{'*.'}{args.ext}")):
+
+    os.chdir(os.path.abspath(args.path))
+    try:
+        os.mkdir(os.path.abspath(args.opath))
+    except: pass
+
+    for pathfile in tqdm(os.listdir()):
         imgs = processing(pathfile)
-        for idx,img in enumerate(imgs):
-            filename,ext = os.path.splitext(pathfile)
-            filename += str(idx)+ext
-            img.save(filename)
+        try:
+            for idx,img in enumerate(imgs):
+                filename,ext = os.path.splitext(pathfile)
+                filename += f"_PROCESSED_{idx}{ext}"
+                opath = f"{args.opath}/{filename}"
+                # print(opath)
+                img.save(opath)
+        except:
+            pass
     
 
 if __name__ == "__main__":
